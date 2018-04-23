d80c6b9655ac61e6912ebd4be6fe64b908066c00
Dmitry Gavrilenko <dmitry.gavrilenko@icloud.com>
2018-04-23 15:40:45 +0300

    Move local_2_global() from Jupyter Notebook

--- code/test_transformations.py
+++ code/test_transformations.py
@@ -2,8 +2,15 @@
 """Unit tests for transformations"""
 
 import unittest
+import cv2
 import numpy as np
+
+#pylint: disable=import-error
+import pandas as pd
+import matplotlib.image as mpimg
+
 import transformations
+
 
 class TestTransformations(unittest.TestCase):
     """Test cases to verify transformations module"""
@@ -23,6 +30,44 @@ class TestTransformations(unittest.TestCase):
         self.assertTrue(np.allclose(
             buf_top[[pts_top[:, 1]], [pts_top[:, 0]]],
             [1.0, 1.0, 1.0, 1.0]))
+
+
+    def test_local_2_global(self):
+        """Points are correctly transformed from the local into
+        map reference frames"""
+
+        robot_log = pd.read_csv(
+            '../test_dataset/robot_log.csv',
+            delimiter=';',
+            decimal='.')
+
+        idx = 0
+
+        xpos = robot_log["X_Position"].values[idx]
+        ypos = robot_log["Y_Position"].values[idx]
+        yaw_deg = robot_log["Yaw"].values[idx]
+        loc_2_glob = transformations.local_2_global(xpos, ypos, yaw_deg)
+
+        img = mpimg.imread(robot_log["Path"].tolist()[idx])
+        img_top = transformations.perspective_2_top(img)
+
+        ground_truth_bw = 255 * mpimg.imread(
+            '../calibration_images/map_bw.png').astype(np.uint8)
+
+        map_piece = cv2.warpAffine(
+            img_top,
+            loc_2_glob,
+            (ground_truth_bw.shape[1], ground_truth_bw.shape[0]))
+
+        map_piece_bw = cv2.cvtColor(map_piece, cv2.COLOR_RGB2GRAY)
+        expected_map_piece = ground_truth_bw * (map_piece_bw != 0)
+
+        correlation = cv2.matchTemplate(
+            map_piece_bw,
+            expected_map_piece,
+            cv2.TM_CCORR_NORMED)[0][0]
+
+        self.assertGreater(correlation, 0.6)
 
 
 if __name__ == '__main__':
--- code/transformations.py
+++ code/transformations.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 
-#pylint: disable=import-error
+# pylint: disable=import-error
 import matplotlib.pyplot as plt
 import cv2
 
@@ -40,6 +40,47 @@ def perspective_2_top(img):
     return warped
 
 
+LOCAL_2_ROVER = np.array([
+    [0.0, -1.0, HEIGHT],
+    [-1.0, 0.0, WIDTH * 0.5]], np.float32)
+
+
+def local_2_global(xpos, ypos, yaw_deg):
+    """Calculate transformation 2x3 affine transformation matrix from
+    local confidence map into global confidence map reference frames"""
+
+    yaw_rad = yaw_deg * np.pi / 180
+    c_scaled = np.cos(yaw_rad) / PIXELS_PER_METER
+    s_scaled = np.sin(yaw_rad) / PIXELS_PER_METER
+
+    local_2_rover_3x3 = np.vstack([LOCAL_2_ROVER, [0.0, 0.0, 1.0]])
+
+    rover_2_global = np.array([
+        [c_scaled, -s_scaled, xpos],
+        [s_scaled, c_scaled, ypos]], np.float32)
+
+    return rover_2_global.dot(local_2_rover_3x3)
+
+
+IMG_ROWS, IMG_COLS = np.indices((HEIGHT, WIDTH))
+IMG_ROWS = IMG_ROWS.ravel()
+IMG_COLS = IMG_COLS.ravel()
+IMG_ONES = np.ones_like(IMG_COLS)
+
+LOCAL_CONF_POINTS = np.vstack([
+    IMG_COLS,
+    IMG_ROWS,
+    IMG_ONES]).astype(np.float32).T
+
+ROVER_CONF_POINTS = LOCAL_2_ROVER.dot(LOCAL_CONF_POINTS.T).T
+
+ROVER_CONF_DIRS = ROVER_CONF_POINTS / np.linalg.norm(
+    ROVER_CONF_POINTS,
+    axis=1).reshape(-1, 1)
+
+np.nan_to_num(ROVER_CONF_DIRS, False)
+
+
 def main():
     """Shows results of what the module does if run as a separate application"""
 
@@ -49,7 +90,18 @@ def main():
     plt.imshow(GRID)
 
     plt.subplot(212)
-    plt.imshow(perspective_2_top(GRID))
+
+    plt.ylim(-160, 160)
+    plt.xlim(-160, 160)
+
+    top_view = perspective_2_top(GRID)
+    top_view_gray = cv2.cvtColor(top_view, cv2.COLOR_RGB2GRAY)
+
+    plt.pcolor(
+        ROVER_CONF_POINTS[:, 0].reshape(top_view_gray.shape),
+        ROVER_CONF_POINTS[:, 1].reshape(top_view_gray.shape),
+        top_view_gray,
+        cmap="gray")
 
     plt.show()
     return
